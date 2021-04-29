1d347a1f7fa773d5bc83bf8ca7be50803ead7d90
rjuliuz <rafa@rafaeljulio.dev>
2021-04-29 07:35:57 -0300

    Finalizacao do Desafio de Refatoracao usando principios do SOLID

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
@@ -19,23 +21,52 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    const currentDate = new Date();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: currentDate,
+      updated_at: currentDate,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const findIndex = this.users.findIndex(
+      (user) => user.id === receivedUser.id
+    );
+
+    if (findIndex !== -1) {
+      const user = this.users[findIndex];
+
+      user.admin = true;
+
+      return user;
+    }
+
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
